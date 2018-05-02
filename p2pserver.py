697ac9839aaa65efb3e42597b4dcb036f9acdf45
rawalshree <rawalshreepal000@gmail.com>
2018-05-02 13:50:50 -0700

    updated server with login check

--- p2pserver.py
+++ p2pserver.py
@@ -1,31 +1,41 @@
 import socket
 from time import sleep
 
+global passwords
+passwords = [('admin', 'pass'), ('DaniWeb', 'best1'), ('newbie', 'help!help!')]
 
-c1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c1.bind(('', 9000))
-c1.listen(5)
+class Server:
 
-c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c2.bind(('', 9001))
-c2.listen(5)
+    def check_login(self, tuple):
+        global passwords
+        if tuple in passwords:
+            return True 
+    
 
-print("Waiting For Connection")
+    def connect(self):
+        c1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c1.bind(('', 9000))
+        c1.listen(5)
 
-while True:
-    try:
-        conn, address = c1.accept()
-        print("Connection from Client : ", address)
-        break
-    except:
-        print("Waiting for user to connect")
-        sleep(3)
+        c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c2.bind(('', 9001))
+        c2.listen(5)
+        print("Waiting For Connection")
 
-while True:
-    try:
-        conn, address = c2.accept()
-        print("Connection from Client : ", address)
-        break
-    except:
-        print("Waiting for user to connect")
-        sleep(3)
+        while True:
+            try:
+                conn, address = c1.accept()
+                print("Connection from Client : ", address)
+                break
+            except:
+                print("Waiting for user to connect")
+                sleep(3)
+
+        while True:
+            try:
+                conn, address = c2.accept()
+                print("Connection from Client : ", address)
+                break
+            except:
+                print("Waiting for user to connect")
+                sleep(3)
